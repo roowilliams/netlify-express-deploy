4c42e5adeda4b1cbd5b6e7a72ee5787734f5b351
Roo Williams <roo@roowilliams.com>
2019-02-17 12:55:53 -0500

    .

--- express/server.js
+++ express/server.js
@@ -10,18 +10,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${
   process.env.DB_HOST
 }/test?retryWrites=true`
 
-const client = new MongoClient(uri, { useNewUrlParser: true })
-console.log(`Connecting to ${uri}`)
-client.connect(err => {
-  console.log('Connected')
-  const collection = client.db('test').collection('devices')
-  // perform actions on the collection object
-  console.log(collection)
-  client.close()
-})
+const dbClient = new MongoClient(uri, { useNewUrlParser: true })
 
 const router = express.Router()
 router.get('/', (req, res) => {
+  console.log('yo')
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.write('<h1>Sup, Express running here!</h1>')
   res.end()
@@ -35,7 +28,18 @@ router.get('/get-ip', (req, res) => {
   // res.json({ route: req.originalUrl })
 })
 
-router.get('/all-ips', (req, res) => res.json({ route: req.originalUrl }))
+router.get('/all-ips', (req, res) => {
+  // get al records from db
+  dbClient.connect(err => {
+    console.log('Connected')
+    const collection = dbClient.db('test').collection('devices')
+    // perform actions on the collection object
+    console.log(collection)
+    dbClient.close()
+  })
+
+  res.json({ route: req.originalUrl })
+})
 
 // post ip address for project name
 router.post('/post-ip', (req, res) => {
@@ -43,9 +47,22 @@ router.post('/post-ip', (req, res) => {
   // save ip address to data store
   const { ip, port, name } = req.body
   const time = Date.now()
+  const entry = { _id: name, ip, port }
+  var response
+  dbClient.connect(err => {
+    dbClient
+      .db('main')
+      .collection('ips')
+      .insertOne(entry, (err, res) => {
+        if (err) return (response = err)
+        response = `${name} mapped to ip address ${ip} on port ${port} at ${time}.`
+      })
+
+    dbClient.close()
+  })
 
   return res.send({
-    response: `${name} mapped to ip address ${ip} on port ${port} at ${time}.`
+    response
   })
 })
 
--- server-local.js
+++ server-local.js
@@ -1,5 +1,4 @@
 'use strict'
 
 const app = require('./express/server')
-
-app.listen(3000, () => console.log('Local app listening on port 3000'))
+app.listen(4000, () => console.log('Local app listening on port 3000'))
